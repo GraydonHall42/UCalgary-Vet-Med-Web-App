4ea3549ad22dffe3562d406420837cc804cd57e9
dyiao <deyliny@gmail.com>
2021-11-16 00:43:51 -0700

    Implemented get, delete, and update profile image methods in ProfileImage

--- springboot-backend-VetSchool/src/main/java/com/springboot/app/springbootbackend/controller/ProfileImageController.java
+++ springboot-backend-VetSchool/src/main/java/com/springboot/app/springbootbackend/controller/ProfileImageController.java
@@ -2,6 +2,7 @@ package com.springboot.app.springbootbackend.controller;
 
 import com.springboot.app.springbootbackend.model.MedicalIssue;
 import com.springboot.app.springbootbackend.model.ProfileImage;
+import com.springboot.app.springbootbackend.model.Weight;
 import com.springboot.app.springbootbackend.service.ProfileImageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,5 +33,30 @@ public class ProfileImageController {
     @GetMapping
     public List<ProfileImage> getAllProfileImages(){
         return profileImageService.getAllProfileImages();
+    }
+
+    // build get weight by id REST API
+    // http://localhost:8080/api/profile-image/1
+    @GetMapping("{id}")
+    public ResponseEntity<ProfileImage> getProfileImageByID(@PathVariable("id") int profileImagesID) {
+        return new ResponseEntity<ProfileImage>(profileImageService.getProfileImageById(profileImagesID), HttpStatus.OK);
+    }
+
+    // build update weight REST API
+    // http://localhost:8080/api/profile-image/1
+    @PutMapping("{id}")
+    public ResponseEntity<ProfileImage> updateProfileImage(@PathVariable("id") int id
+            , @RequestBody ProfileImage profileImage){
+        return new ResponseEntity<ProfileImage>(profileImageService.updateProfileImage(profileImage, id), HttpStatus.OK);
+    }
+
+    // build delete weight REST API
+    // http://localhost:8080/api/profile-image/1
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteProfileImage(@PathVariable("id") int id) {
+
+        profileImageService.deleteProfileImage(id);
+
+        return new ResponseEntity<String>("Weight deleted successfully!", HttpStatus.OK);
     }
 }
--- springboot-backend-VetSchool/src/main/java/com/springboot/app/springbootbackend/service/impl/ProfileImageServiceImpl.java
+++ springboot-backend-VetSchool/src/main/java/com/springboot/app/springbootbackend/service/impl/ProfileImageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.springboot.app.springbootbackend.service.impl;
 
+import com.springboot.app.springbootbackend.exception.ResourceNotFoundException;
 import com.springboot.app.springbootbackend.model.ProfileImage;
+import com.springboot.app.springbootbackend.model.Weight;
 import com.springboot.app.springbootbackend.repository.ProfileImageRepository;
 import com.springboot.app.springbootbackend.service.ProfileImageService;
 import org.springframework.stereotype.Service;
@@ -25,5 +27,37 @@ public class ProfileImageServiceImpl implements ProfileImageService {
     @Override
     public List<ProfileImage> getAllProfileImages() {
         return profileImageRepository.findAll();
+    }
+
+    @Override
+    public ProfileImage getProfileImageById(int id) {
+        return profileImageRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("ProfileImage", "Id", id));
+    }
+
+    @Override
+    public ProfileImage updateProfileImage(ProfileImage profileImage, int id) {
+
+        // check whether profile image with given id exists in DB or not
+        ProfileImage existingProfileImage = profileImageRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("ProfileImage", "Id", id));
+
+        existingProfileImage.setAnimalId(profileImage.getAnimalId());
+        existingProfileImage.setDate(profileImage.getDate());
+        existingProfileImage.setImage(profileImage.getImage());
+
+        // save existing weight to DB
+        profileImageRepository.save(existingProfileImage);
+        return existingProfileImage;
+    }
+
+    @Override
+    public void deleteProfileImage(int id) {
+
+        // check whether a profile image exist in a DB or not
+        profileImageRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("ProfileImage", "Id", id));
+
+        profileImageRepository.deleteById(id);
     }
 }
