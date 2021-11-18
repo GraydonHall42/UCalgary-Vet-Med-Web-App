2376172ef261580c515f0da0752e5ac9d6557fa7
Jared Kraus <jared.kraus3@gmail.com>
2021-11-18 14:21:50 -0700

    fix(react-app): E248-86

---
+++ react-frontend-vetschool/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="logo">
+            </div>
+            <div className="links"></div>
+            
+        </div>
+    )
+}
+
+export default Navbar
