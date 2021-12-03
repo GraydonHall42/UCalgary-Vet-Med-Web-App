2af36284f4fd35e6369be613c9e2232e3bd5f5e0
GraydonHall42 <gwhall@ualberta.ca>
2021-12-02 18:55:01 -0700

    feat(login-logic)E248-113\n created functionality for logging ing

---
+++ react-frontend-vetschool/src/components/AutorizedRoutes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import NavigationBar from "./NavigationBar";
+import Home from "../pages/Home";
+import StatusDashboard from "../pages/StatusDashboard";
+import AnimalProfilePage from "../pages/AnimalProfilePage";
+import UserManagement from "../pages/UserManagement";
+import AnimalBooking from "../pages/AnimalBooking";
+import UserProfilePage from "../pages/UserProfilePage";
+import IndividualMedicalIssuePage from "../pages/IndividualMedicalIssuePage";
+
+function AuthorizedRoutes() {
+    return (
+        <div className="auth">
+            <Router>
+                <NavigationBar/>
+                <Routes className="routes">
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/status" element={<StatusDashboard/>}/>
+                    <Route exact path="/animal-profile" element={<AnimalProfilePage/>}/>
+                    <Route exact path="/user-management" element={<UserManagement/>}/>
+                    <Route exact path="/booking" element={<AnimalBooking/>}/>
+                    <Route exact path="/user-profile" element={<UserProfilePage/>}/>
+                    <Route exact path="/medical-issue/3" element={<IndividualMedicalIssuePage/>}/>
+                </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default AuthorizedRoutes
