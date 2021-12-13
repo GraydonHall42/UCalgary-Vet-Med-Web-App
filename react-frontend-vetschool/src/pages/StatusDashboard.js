3ec40d4afd66fb273a2880b232937fa78381f66a
GraydonHall42 <gwhall@ualberta.ca>
2021-12-13 09:15:24 -0700

    feat(animal-status-dash)E248-106\n Add logic to animal status dash

--- react-frontend-vetschool/src/pages/StatusDashboard.js
+++ react-frontend-vetschool/src/pages/StatusDashboard.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import "../styles/StatusDashboard.css"
+
 
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
@@ -7,9 +7,12 @@ import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.m
 
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
-import {Container} from "react-bootstrap";
+import {Button, Container, Image} from "react-bootstrap";
 import axios from "axios";
 import useAuthorization from '../hooks/useAuthorization';
+import {Link} from "react-router-dom";
+import "../styles/StatusDashboard.css"
+import filterFactory, { selectFilter, textFilter } from 'react-bootstrap-table2-filter';
 
 function StatusDashboard() {
 
@@ -22,6 +25,7 @@ function StatusDashboard() {
         axios.get('http://localhost:8080/api/medical/active', config)
             .then(res => {
                 console.log(res.data)
+                setMedicalIssues(res.data)
                 setLoading(false)
             })
             .catch(err => {
@@ -29,36 +33,113 @@ function StatusDashboard() {
             })
     }
 
-
     useEffect(() => {
         getActiveMedicalIssues();
     }, []);  // empty array so only executes once
 
-    const productsGenerator = quantity => {
-        const items = [];
-        for (let i = 0; i < quantity; i++) {
-            items.push({ id: i, name: `Item name ${i}`, price: 2100 + i });
-        }
-        return items;
-    };
 
-    const products = productsGenerator(100);
+
+    function imageFormatter(cell, row){
+        return (<Image style={{width:50, height:50, borderRadius:"2px"}} src={cell}/>)
+    }
+
+    function profileButton(cell, row){
+        return (
+            <div className={"m-auto"}>
+                <Link to={"/animal-profile/"+cell}>
+                    <Button className={"searchResultBtn"} variant={"danger"}>
+                        Profile
+                    </Button>
+                </Link>
+                <Link to={"/medical/"+row.medicalIssueId}>
+                    <Button className={"searchResultBtn"} variant={"warning"}>
+                        Medical Issue
+                    </Button>
+                </Link>
+            </div>
+        )
+    }
+
+    const selectOptions = {
+        0: 'Healthy',
+        1: 'Attention',
+        2: 'Urgent'
+    };
 
     const columns = [
         {
-            dataField: "id",
-            text: "Product ID",
-            sort: true
+            dataField: "animal.profilePhoto",
+            text: "Profile Photo",
+            formatter:imageFormatter,
+            headerStyle: (colum, colIndex) => {
+                return { width: '10%', textAlign: 'center' };
+            }
         },
         {
-            dataField: "name",
-            text: "Product Name",
-            sort: true
+            dataField: "animal.animalName",
+            text: "Animal Name",
+            headerStyle: (colum, colIndex) => {
+                return { width: '10%', textAlign: 'center'};
+            }
         },
         {
-            dataField: "price",
-            text: "Product Price"
-        }
+            dataField: "animal.animalType",
+            text: "Animal Type",
+            headerStyle: (colum, colIndex) => {
+                return { width: '10%', textAlign: 'center' };
+            },
+            filter: textFilter({
+                className: 'test-classname',
+                placeholder: 'Filter Criteria',
+                style: {
+                    textAlign: 'center'
+                },
+            })
+        },
+        {
+            dataField: "issueName",
+            text: "Issue Name",
+            headerStyle: (colum, colIndex) => {
+                return { width: '10%', textAlign: 'center' };
+            },
+        },
+        {
+            dataField: "animal.status",
+            text: "Animal Status",
+            headerStyle: (colum, colIndex) => {
+                return { width: '10%', textAlign: 'center' };
+            },
+            filter: textFilter({
+                className: 'test-classname',
+                placeholder: 'Filter Criteria',
+                style: {
+                    textAlign: 'center'
+                },
+            })
+        },
+        {
+            dataField: "currentStatus",
+            text: "Issue Status",
+            headerStyle: (colum, colIndex) => {
+                return { width: '9%', textAlign: 'center' };
+            },
+            filter: textFilter({
+                className: 'test-classname',
+                placeholder: 'Filter Criteria',
+                style: {
+                    textAlign: 'center'
+                },
+            })
+        },
+        {
+            dataField: "animal.animalId",
+            text: "Navigate To",
+            formatter:profileButton,
+            align: 'center',
+            headerStyle: (colum, colIndex) => {
+                return { width: '20%', textAlign: 'center' };
+            },
+        },
     ];
 
     // while awaiting axios...
@@ -73,10 +154,12 @@ function StatusDashboard() {
         <div className={"statusDash"}>
             <BootstrapTable
                 bootstrap4
-                keyField="id"
-                data={products}
+                keyField="medicalIssueId"
+                data={medicalIssues}
                 columns={columns}
                 pagination={paginationFactory({ hideSizePerPage : true })}
+                striped={true}
+                filter={filterFactory()}
             />
         </div>
         </Container>
